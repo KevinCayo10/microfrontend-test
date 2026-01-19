d9b8238ccbc6c78cc8b0bbe1a21c50b2f9c244c4
Kevin CT <kejoelct1002@gmail.com>
2026-01-19 14:03:52 -0500

    feat: valiadciones backend libreria joi

--- backend/src/insfraestructure/adapters/routes/menu.routes.ts
+++ backend/src/insfraestructure/adapters/routes/menu.routes.ts
@@ -2,6 +2,13 @@ import { Router } from "express";
 import { MenuImplement } from "../repository/menu.repository";
 import { MenuService } from "../../../application/services/menu.service";
 import { MenuController } from "../controllers/menu.controller";
+import {
+  createMenuSchema,
+  updateMenuSchema,
+  idParamSchema,
+  validateBody,
+  validateParams,
+} from "../../validators/menu.validator";
 
 const menuRepository = new MenuImplement();
 const menuService = new MenuService(menuRepository);
@@ -12,13 +19,30 @@ export class MenuRoutes {
   static getRoutes(): Router {
     this.routes.get("/", menuController.findAll.bind(menuController));
 
-    this.routes.post("/", menuController.create.bind(menuController));
+    this.routes.post(
+      "/",
+      validateBody(createMenuSchema),
+      menuController.create.bind(menuController),
+    );
 
-    this.routes.patch("/:id", menuController.update.bind(menuController));
+    this.routes.patch(
+      "./:id".replace("./", "/"),
+      validateParams(idParamSchema),
+      validateBody(updateMenuSchema),
+      menuController.update.bind(menuController),
+    );
 
-    this.routes.get("/:id", menuController.findById.bind(menuController));
+    this.routes.get(
+      "./:id".replace("./", "/"),
+      validateParams(idParamSchema),
+      menuController.findById.bind(menuController),
+    );
 
-    this.routes.delete("/:id", menuController.delete.bind(menuController));
+    this.routes.delete(
+      "/:id",
+      validateParams(idParamSchema),
+      menuController.delete.bind(menuController),
+    );
 
     return this.routes;
   }
